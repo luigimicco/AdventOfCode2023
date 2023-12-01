9c048d8a10f1837c32c2e155a1a9bdc8a2f936cd
luigimicco <luigi.micco@gmail.com>
2023-12-01 22:05:27 +0100

    fix calcolo

--- level 1/js/script.js
+++ level 1/js/script.js
@@ -1,3 +1,4 @@
+console.time();
 const space = document.getElementById("space");
 
 const input = space.innerHTML;
@@ -12,41 +13,41 @@ let sum = 0;
 const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const verbose = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
+rows.forEach((row, number) => {
+  row = row.trim()
+  if (row != "") {
 
-for (let i = 0; i < rows.length; i++) {
-  let row = rows[i].trim()
+    let first = -1;
+    let last = 0;
 
-  if (row == "") continue;
-
-  let first = -1;
-  let last = 0;
-
-  let newRow = ""
-  for (let j = 0; j < row.length; j++) {
-
-
-    for (let k = 0; k < verbose.length; k++) {
-      if (row.substring(j, j + verbose[k].length) == verbose[k]) {
-        newRow = newRow + digits[k];
-        k = k + verbose[k].length;
+    let newRow = ""
+    for (let j = 0; j < row.length; j++) {
+      if (digits.includes(row[j])) {
+        newRow += row[j];
       } else {
-        newRow = newRow + row[j];
-
+        for (let k = 0; k < verbose.length; k++) {
+          if (row.substring(j, j + verbose[k].length) == verbose[k]) {
+            newRow += digits[k];
+            j += verbose[k].length - 1;
+          }
+        }
+      }
+    }
+    console.log(newRow);
+    for (let j = 0; j < newRow.length; j++) {
+      if (digits.includes(newRow[j])) {
+        if (first == -1) first = newRow[j];
+        last = newRow[j];
       }
 
     }
+
+    sum = sum + parseInt(first) * 10 + parseInt(last);
+    console.log(number, sum, parseInt(first) * 10 + parseInt(last))
   }
+})
 
 
-  for (let j = 0; j < newRow.length; j++) {
-    if (digits.includes(newRow[j])) {
-      if (first == -1) first = newRow[j];
-      last = newRow[j];
-    }
-
-  }
-  console.log(first, last)
-  sum = sum + parseInt(first) * 10 + parseInt(last);
-}
 console.log(sum);
 
+console.timeEnd();
